10a1c7db715bedb5d65c971e2ddc2f4319aadb32
puraxa <edinpurivatra@gmail.com>
2019-05-16 10:12:10 +0200

    added browsersync and made it work on every change

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ let minify = require('gulp-html-minifier2');
 let uglify = require('gulp-uglify-es').default;
 let cleancss = require('gulp-clean-css');
 let ts = require('gulp-typescript');
+let browserSync = require('browser-sync');
 
 let tsProject = ts.createProject('tsconfig.json');
 
@@ -24,5 +25,25 @@ gulp.task('ts', async function(){
         .pipe(uglify())
         .pipe(gulp.dest('./dist'));
 });
+
+gulp.task('server', async function(){
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+})
+
+gulp.task('reload',async function(){
+    browserSync.reload();
+})
+
+gulp.task('watch', function(){
+    gulp.watch('./src/*.ts', gulp.series('ts', 'reload'));
+    gulp.watch('./src/*.html', gulp.series('minify', 'reload'));
+    gulp.watch('./src/*.css', gulp.series('cleancss', 'reload'));
+});
+
+gulp.task('browsersync', gulp.parallel('server','watch'));
 
 gulp.task('default', gulp.parallel(['minify', 'cleancss', 'ts']));
